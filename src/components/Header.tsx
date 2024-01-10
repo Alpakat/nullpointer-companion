2c4f2024353bf66422297eac35277efb7725d0ac
Alpakat <alpakat2@gmail.com>
2024-01-10 22:49:41 +0100

    small changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import styles from './Header.module.css'
 
 export default function Header() {
 	return (
-		<div className={styles.main}>
-			<h1>Nullpointer</h1>
-            <h1>- The Game</h1>
+		<div className={styles.container}>
+			<div className={styles.main}>
+				<h1>Nullpointer</h1>
+				<h1>- The Game</h1>
+			</div>
 		</div>
 	)
 }
--- src/components/Settings/SettingsManager.tsx
+++ src/components/Settings/SettingsManager.tsx
@@ -13,35 +13,36 @@ export default function SettingsManager() {
 	}
 
 	return (
-		<div className={styles.main}>
-			<h3 style={{ margin: 0 }}>Schwierigkeit</h3>
+		<>
+			<div className={styles.main}>
+				<h3 style={{ margin: 0 }}>Schwierigkeit</h3>
 
-			<div className={styles.mutliSelector}>
-				<button className={styles.mutliSelectorButton} onClick={() => setSelectedOption('Schwach')}>
-					<span>Schwach</span>
-					<div className={classNames(styles.mutliSelectorButtonBackground, selectedOption == 'Schwach' && styles.mutliSelectorButtonBackgroundSelected)}></div>
-				</button>
-				<button className={styles.mutliSelectorButton} onClick={() => setSelectedOption('Schwer')}>
-					<span>Schwer</span>
-					<div className={classNames(styles.mutliSelectorButtonBackground, selectedOption == 'Schwer' && styles.mutliSelectorButtonBackgroundSelected)}></div>
-				</button>
-				<button className={styles.mutliSelectorButton} onClick={() => setSelectedOption('Schwierig')}>
-					<span>Schwierig</span>
-					<div className={classNames(styles.mutliSelectorButtonBackground, selectedOption == 'Schwierig' && styles.mutliSelectorButtonBackgroundSelected)}></div>
-				</button>
+				<div className={styles.mutliSelector}>
+					<button className={styles.mutliSelectorButton} onClick={() => setSelectedOption('Schwach')}>
+						<span>Schwach</span>
+						<div className={classNames(styles.mutliSelectorButtonBackground, selectedOption == 'Schwach' && styles.mutliSelectorButtonBackgroundSelected)}></div>
+					</button>
+					<button className={styles.mutliSelectorButton} onClick={() => setSelectedOption('Schwer')}>
+						<span>Schwer</span>
+						<div className={classNames(styles.mutliSelectorButtonBackground, selectedOption == 'Schwer' && styles.mutliSelectorButtonBackgroundSelected)}></div>
+					</button>
+					<button className={styles.mutliSelectorButton} onClick={() => setSelectedOption('Schwierig')}>
+						<span>Schwierig</span>
+						<div className={classNames(styles.mutliSelectorButtonBackground, selectedOption == 'Schwierig' && styles.mutliSelectorButtonBackgroundSelected)}></div>
+					</button>
+				</div>
+
+				{/* <div className={styles.divider}></div> */}
+
+				<h3 style={{ margin: 0 }}>Spieler (1-3)</h3>
+
+				<input className={styles.input} type="text" name="player1" value={playerNames.player1} onChange={handleInputChange} />
+				<input className={styles.input} type="text" name="player2" value={playerNames.player2} onChange={handleInputChange} />
+				<input className={styles.input} type="text" name="player3" value={playerNames.player3} onChange={handleInputChange} />
 			</div>
-
-            <br />
-
-			<h3 style={{ margin: 0 }}>Spieler (1-3)</h3>
-
-			<input className={styles.input} type="text" name="player1" value={playerNames.player1} onChange={handleInputChange} />
-			<input className={styles.input} type="text" name="player2" value={playerNames.player2} onChange={handleInputChange} />
-			<input className={styles.input} type="text" name="player3" value={playerNames.player3} onChange={handleInputChange} />
-
-			<div className={styles.divider}></div>
-
-			<button className={styles.button}>Spiel starten</button>
-		</div>
+			<div className={styles.main}>
+				<button className={styles.button}>Spiel starten</button>
+			</div>
+		</>
 	)
 }
