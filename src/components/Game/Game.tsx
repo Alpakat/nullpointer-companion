aeff1a32ff98616336e41446a4d43fe1295f16a4
Alpakat <alpakat2@gmail.com>
2024-01-11 21:47:26 +0100

    matches

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,6 +3,7 @@
 import classNames from 'classnames'
 import styles from './Game.module.css'
 import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 
 const newNumberFunction = (difficulty: 'Schwach' | 'Schwer' | 'Schwierig') => {
 	const optionsSchwach = [0, 1, 1, 3, 2, 2]
@@ -22,6 +23,9 @@ export default function Game({ difficulty, players }: { difficulty: 'Schwach' | 
 
 	const [numbers, setNumbers] = useState<number[]>([])
 	const [colors, setColors] = useState<number[]>([])
+
+	const [matchWin, setMatchWin] = useState<number | undefined>(undefined)
+	const [matchDisplay, setMatchDisplay] = useState<boolean>(false)
 
 	useEffect(() => {
 		const newNumber = newNumberFunction(difficulty)
@@ -51,6 +55,23 @@ export default function Game({ difficulty, players }: { difficulty: 'Schwach' | 
 		setCurrentRound((e) => e + 1)
 	}
 
+	const playingPlayers = players.filter((player) => player != undefined && player != '')
+
+	const currentlyPlayingPlayer = playingPlayers[currentRound % playingPlayers.length]
+	const currentlyPlayingPlayerIndex = players.indexOf(currentlyPlayingPlayer)
+
+	const match = (player: number) => {
+		//select random player of the player that is playing and the player that is in propertys
+		const playersToConsider = [currentlyPlayingPlayerIndex, player]
+		const randomPlayer = playersToConsider[Math.floor(Math.random() * playersToConsider.length)]
+
+		setMatchWin(randomPlayer)
+		setMatchDisplay(true)
+		setTimeout(() => {
+			setMatchDisplay(false)
+		}, 1000)
+	}
+
 	return (
 		<div>
 			<div className={styles.main}>
@@ -68,14 +89,17 @@ export default function Game({ difficulty, players }: { difficulty: 'Schwach' | 
 							return null
 						}
 
-						const playingPlayers = players.filter((player) => player != undefined && player != '').length
 						//should only count players that are actually playing
 						const actualIndex = players.slice(0, index).filter((player) => player != undefined && player != '').length
 
 						return (
 							<div className={styles.playerDisplay} key={index}>
 								<div
-									className={classNames(styles.playerColor, styles['playerColor' + index.toString()], currentRound % playingPlayers == actualIndex && styles.playerColorActive)}
+									className={classNames(
+										styles.playerColor,
+										styles['playerColor' + index.toString()],
+										currentRound % playingPlayers.length == actualIndex && styles.playerColorActive
+									)}
 								></div>
 								<h2 style={{ margin: 0 }}>{player}</h2>
 							</div>
@@ -109,7 +133,30 @@ export default function Game({ difficulty, players }: { difficulty: 'Schwach' | 
 						Weiter
 					</button>
 				</div>
-				<button className={classNames(styles.button, styles.matchButton)}>Match</button>
+			</div>
+			<div className={classNames(styles.main, styles.matchContainer)}>
+				<motion.div animate={{ filter: matchDisplay ? 'blur(8px) saturate(0.3) brightness(0.5)' : 'blur(0px) saturate(1) brightness(1)' }} className={styles.matchOverlay}>
+					{players.map((player, index) => {
+						if(player == undefined || player == '') return null
+						return (
+							<button
+								className={classNames(styles.button, styles['button' + index.toString()], currentlyPlayingPlayerIndex == index && styles.disabledButton)}
+								disabled={currentlyPlayingPlayerIndex == index || matchDisplay}
+								key={index}
+								onClick={() => match(index)}
+							>
+								<span style={{ margin: 0, fontWeight: 600 }}>
+									<span style={{ fontWeight: 300 }}>Match gegen</span> {player}
+								</span>
+							</button>
+						)
+					})}
+				</motion.div>
+				<motion.div initial={{ opacity: 0 }} animate={{ opacity: matchDisplay ? 1 : 0 }} className={styles.matchOverlay} style={{ zIndex: 5, pointerEvents: 'none' }}>
+					<h1>
+						<span style={{ fontWeight: 300 }}>Gewinner:</span> {players[matchWin ?? 0]}
+					</h1>
+				</motion.div>
 			</div>
 		</div>
 	)
